c225390bad48caa11d1c78b1b6ea81489071c04d
Subhashini <subhashini.m@lucidatechnologies.com>
2025-05-15 12:29:11 +0530

    FEAT:New features added

--- src/Redux/userSlice.js
+++ src/Redux/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchUsers = createAsyncThunk(
-  "users/fetchUsersDetail",
+  "users/fetchUsers",
   async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await response.json();
